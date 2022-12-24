c52f7af8809611da89ac65eea952eebf6543d53a
satywanbhole <satywanbhole@gmail.com>
2022-12-24 18:16:54 +0530

    Second commit: added new LinkedList

---
+++ src/com/git/program/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package com.git.program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Nitin");
+		list.add("Samrudhi");
+		
+		System.out.println(list);
+		
+	}
+}
