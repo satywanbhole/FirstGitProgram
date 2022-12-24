4e76cd2af4b05e4d6b64fcc62537817880e6c090
satywanbhole <satywanbhole@gmail.com>
2022-12-24 17:48:20 +0530

    First commit : added FirstArrayList code

---
+++ src/com/git/program/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.git.program;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Amol");
+		list.add("Satywan");
+		
+		System.out.println(list);
+		
+	}
+}
